6d8dc8899d2662ed68f09848d1e8dce88b65f666
tiedsandi <fachransandi@gmail.com>
2024-01-09 07:37:39 +0700

    adding middleware

--- learning/app.js
+++ learning/app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('in the middleware!');
+  next(); // allows the req to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log('that is middleware!');
+  res.send('<h1>Helo from express!</h1>');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
