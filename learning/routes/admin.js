3abcdd94f53791259af59b3b9bd36f572aec3430
tiedsandi <fachransandi@gmail.com>
2024-01-21 19:24:06 +0700

    sharing data across req & users

--- learning/routes/admin.js
+++ learning/routes/admin.js
@@ -7,22 +7,16 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  const message = req.body.title;
-
-  fs.writeFile('message.txt', message, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Internal Server Error');
-    }
-
-    res.redirect('/');
-  });
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
