65d811bf2816be16abc30614363f10312794ba24
tiedsandi <fachransandi@gmail.com>
2024-01-25 08:39:23 +0700

    converting project to Handlebars

--- learning/routes/shop.js
+++ learning/routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: 'Shopee', path: '/' });
+  res.render('shop', { prods: products, pageTitle: 'Shopee', path: '/', hasProducts: products.length > 0 });
 });
 
 module.exports = router;
