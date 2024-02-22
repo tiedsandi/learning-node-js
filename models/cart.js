60168342866ce995f47147785d62b0baf1cbf3d2
tiedsandi <fachransandi@gmail.com>
2024-02-22 09:42:50 +0700

    fixing bug delete product

--- models/cart.js
+++ models/cart.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json');
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'cart.json');
 
 module.exports = class Cart {
   static addProduct(id, productPrice) {
+    // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
       let cart = {products: [], totalPrice: 0};
       if (!err) {
         cart = JSON.parse(fileContent);
       }
+      // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex((prod) => prod.id === id);
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-
+      // Add new product/ increase quantity
       if (existingProduct) {
         updatedProduct = {...existingProduct};
         updatedProduct.qty = updatedProduct.qty + 1;
@@ -32,9 +34,11 @@ module.exports = class Cart {
 
   static deleteProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
-      if (err) return;
+      if (err) {
+        return;
+      }
       const updatedCart = {...JSON.parse(fileContent)};
-      const product = updatedCart.products.findIndex((prod) => prod.id === id);
+      const product = updatedCart.products.find((prod) => prod.id === id);
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter((prod) => prod.id !== id);
       updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
@@ -42,6 +46,17 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err);
       });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
     });
   }
 };
