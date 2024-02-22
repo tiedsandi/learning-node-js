88300b9b8eddc4d01945d46f8734d4bf9d04f95d
tiedsandi <fachransandi@gmail.com>
2024-02-22 09:41:00 +0700

    deleting product and also deleting cart items

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
 
 const getProductsFromFile = (cb) => {
@@ -38,6 +40,16 @@ module.exports = class Product {
           console.log(err);
         });
       }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const product = products.find((prod) => prod.id === id);
+      const updatedProduct = products.filter((prod) => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProduct), (err) => {
+        if (!err) Cart.deleteProduct(id, product.price);
+      });
     });
   }
 
